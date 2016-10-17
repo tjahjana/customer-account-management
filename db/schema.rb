b3e17c302a05ee83c4bd5daa2351fc9ae5248a6d
Anja Kammer <post@anjakammer.de>
2016-10-17 22:49:47 +0200

    added users entity by hand

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017170835) do
+ActiveRecord::Schema.define(version: 20161017170836) do
 
   create_table "contracts", force: :cascade do |t|
     t.string   "type"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20161017170835) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "fee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
