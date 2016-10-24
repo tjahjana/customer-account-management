ffe7bdea792f1f460ea49b279fede45a0748234f
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-10-25 00:31:09 +0200

    update view and controller for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -69,6 +69,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:email, :password, :password_confirmation)
+      params.require(:customer).permit(:email, :password, :password_confirmation, :name, :address, :tax)
     end
 end
