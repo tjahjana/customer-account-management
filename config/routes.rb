e029002d731a8229db81745bbe94ff73472ca83d
Anja Kammer <post@anjakammer.de>
2016-10-25 21:00:08 +0200

    remove item resource from routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #get 'items/edit'
 
   resources :customers
-  resources :items
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
