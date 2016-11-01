79a741962f3e3b16d6d977862022d416c33a25b1
Anja Kammer <post@anjakammer.de>
2016-11-01 18:57:58 +0100

    added model test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @customer = customers(:one)
+    @contract = contracts(:one)
+  end
+  test "make sure association works here" do
+    assert_equal 1, @customer.contracts.size
+    assert_equal @customer.contracts.where(:id => 1).first, @contract
+  end
+
 end
