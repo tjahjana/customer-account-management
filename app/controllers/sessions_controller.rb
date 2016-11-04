a90ec4d1f26ecaba54ff52e0cf0ed391dcd2f430
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-11-04 13:32:52 +0100

    implement login in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user.try(:authenticate, params[:session][:password])
+      log_in(user)
+      redirect_to root_url
+      flash[:success] = "You are logged in"
+    else
+      redirect_to login_url
+      flash[:error] = "Invalid username/password combination"
+    end
   end
 
   def destroy
+  	log_out
+  	redirect_to root_url
+  	flash[:success] = "You are logged out"
   end
 end
