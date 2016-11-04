2aeaf4b8761722b39677df19fd64369400021b0f
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-11-04 13:31:03 +0100

    bug fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
-  get '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   root 'customers#index'
